f92bdee3b3895418be5f65bef0e0779c500647fd
jackie <jackieliu_cornell@163.com>
2021-01-18 18:24:33 +0800

    small bug fix

--- esmm/prepare_tfrecord.sh
+++ esmm/prepare_tfrecord.sh
@@ -3,12 +3,12 @@
 #download train and test data from s3
 mkdir -p ./dy_train
 mkdir -p ./dy_test
-mkdir -p ./data_tfrecord
+mkidr -p ./data_tfrecord
 
 aws s3 cp s3://douyu-datalab/train_1221.csv/ ./dy_train --recursive
 aws s3 cp s3://douyu-datalab/test_1221.csv/ ./dy_test --recursive
 
 #process train
-python prepare_tfrecord.py -o='./data_tfrecord/train.tfrecord' -i='./dy_train'
+python prepare_tfrecord.py --output_path='./data_tfrecord./train.tfrecord' --input_folder='./dy_train'
 #process test
-python prepare_tfrecord.py -o './data_tfrecord/test.tfrecord' -i './dy_test'
+python prepare_tfrecord.py --output_path='./data_tfrecord/test.tfrecord' --input_folder='./dy_test'
